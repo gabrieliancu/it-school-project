c98bf67bd7b806eb1b6162320c4bca2831b42a0e
gabrieli <igabrielcatalin@gmail.com>
2025-10-27 19:11:39 +0200

    new commit

--- HotelBooking/src/main/java/com/example/HotelBooking/HotelBookingApplication.java
+++ HotelBooking/src/main/java/com/example/HotelBooking/HotelBookingApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "Hotel Reservation API", version = "1.0", description = " API pentru rezervarile unui hotel"))
+@OpenAPIDefinition(info = @Info(title = "Hotel Reservation API", version = "1.0", description = " API pentru rezervarile hoteliere"))
 @SpringBootApplication
 
 public class HotelBookingApplication {
